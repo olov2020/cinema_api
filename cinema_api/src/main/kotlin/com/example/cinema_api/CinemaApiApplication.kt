102296b04dea2d1b6cd229e1ed42acc53957ac52
Vladimir Vinogradov <vavinogradov@edu.hse.ru>
2024-03-02 14:37:14 +0300

    initial

---
+++ cinema_api/src/main/kotlin/com/example/cinema_api/CinemaApiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.cinema_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CinemaApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<CinemaApiApplication>(*args)
+}
